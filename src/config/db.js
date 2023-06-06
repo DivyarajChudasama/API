c08b725d683c552a86b52f3ce1a5d4ba91c35400
DivyarajChudasama <92981225+DivyarajChudasama@users.noreply.github.com>
2023-06-06 17:28:27 +0530

    Add files via upload

---
+++ src/config/db.js
@@ -0,0 +1,5 @@
+module.exports = {
+    url: "mongodb+srv://divyarajc619:divyaraj30@cluster0.qgpsdna.mongodb.net/",
+    database: "api",
+    imgBucket: "image",
+  };
---
+++ src/server.js
@@ -0,0 +1,17 @@
+const cors = require("cors");
+const express = require("express");
+const app = express();
+const initRoutes = require("./routes");
+
+var corsOptions = {
+  origin: "http://localhost:8081"
+};
+
+app.use(cors(corsOptions));
+app.use(express.urlencoded({ extended: true }));
+initRoutes(app);
+
+let PORT = process.env.PORT || 8080;
+app.listen(PORT, () => {
+  console.log(`Running at localhost:${PORT}`);
+});
