734133fa68a05f24117c846c1bedb3ac9c4e61cc
Divyarajsinh Chudasama <92981225+DivyarajChudasama@users.noreply.github.com>
2023-06-12 11:48:10 +0530

    Update upload.js

--- src/controllers/upload.js
+++ src/controllers/upload.js
@@ -6,7 +6,7 @@ const GridFSBucket = require("mongodb").GridFSBucket;
 
 const url = dbConfig.url;
 
-const baseUrl = "http://localhost:8080/files/";
+const baseUrl = "https://image-api-90tl.onrender.com/";
 
 const mongoClient = new MongoClient(url);
 
