76c190daf8cae64a595b0baa6e3fb6574932065a
Divyarajsinh Chudasama <92981225+DivyarajChudasama@users.noreply.github.com>
2023-06-13 14:30:52 +0530

    Update upload.js

--- src/middleware/upload.js
+++ src/middleware/upload.js
@@ -9,8 +9,9 @@ var storage = new GridFsStorage({
   file: (req, file) => {
     const match = ["image/png", "image/jpeg","video/x-matroska", "image/gif","application/pdf","video/mp4","application/zip","application/x-rar-compressed",
                    "application/x-dc","model/ifc","image/vnd.dwg","image/bmp","application/vnd.ms-excel","text/plain","audio/aac","application/vnd.ms-powerpoint","application/vnd.openxmlformats-officedocument.presentationml.presentation"
-                    "text/csv","application/msword","application/vnd.openxmlformats-officedocument.wordprocessingml.document","application/java-archive","	application/json","audio/mpeg"
-                     "video/mpeg","audio/mpeg","application/vnd.ms-excel","application/vnd.openxmlformats-officedocument.spreadsheetml.sheet","application/xml"];
+                    ,"text/csv","application/msword","application/vnd.openxmlformats-officedocument.wordprocessingml.document","application/java-archive","application/json","audio/mpeg",
+                     "video/mpeg","audio/mpeg","application/vnd.ms-excel","application/vnd.openxmlformats-officedocument.spreadsheetml.sheet","application/xml",
+                      "application/octet-stream"];
 
     if (match.indexOf(file.mimetype) === -1) {
       const filename = `${file.originalname}`;
